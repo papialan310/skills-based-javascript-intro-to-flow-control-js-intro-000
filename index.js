12bdf5e72d9a2f7da14fd2dc36b1b65c16bdf154
papialaan310 <alan.alejandro911@gmail.com>
2016-11-01 17:56:34 +0000

    I'm ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,29 @@
+// write your code below!
+function basicTeenager(age) {
+if (age >= 13 && age <= 19){
+  return "You are a teenager!";
+}
+}
+
+function teenager(age) {
+if (age >= 13 && age <= 19){
+  return "You are a teenager!";
+} else
+  return "You are not a teenager";
+}
+
+function ageChecker(age) {
+  if (age >= 13 && age <= 19){
+  return "You are a teenager!";
+}else
+  return"You are a kid";
+}
+
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+}
